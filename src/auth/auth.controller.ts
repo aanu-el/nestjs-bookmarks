043c9b589f8b6954f29bb5042389bee8865b073b
Peace Aanu <peaceaanu247@gmail.com>
2023-12-14 14:28:26 +0100

    sigup logic

---
+++ src/auth/auth.controller.ts
@@ -0,0 +1,20 @@
+/* eslint-disable prettier/prettier */
+import { Controller, Post, Body } from "@nestjs/common";
+import { AuthService } from "./auth.service";
+import { AuthDto } from "./dto";
+
+@Controller('auth')
+export class AuthController {
+    constructor(private authService: AuthService) { }
+
+    @Post('signup')
+    signup(@Body() dto: AuthDto) {
+        return this.authService.signup(dto)
+    }
+
+    @Post('signin')
+    signin() {
+        return this.authService.signin()
+    }
+
+}
